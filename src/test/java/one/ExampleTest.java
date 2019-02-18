3f01f20d7728c9f66957e5fe3efa4f13194be887
Dmitri Styf <Dmitri.styf@gmail.com>
2019-02-18 20:51:39 +0200

    1st

---
+++ src/test/java/one/ExampleTest.java
@@ -0,0 +1,13 @@
+package one;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class ExampleTest {
+
+    @Test
+    public void nameME() {
+        assertTrue(true);
+    }
+}
